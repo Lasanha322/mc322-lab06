993c31d744982a163f4058d683aa6f07c94e0972
CauanNewton <63312517+CauanNewton@users.noreply.github.com>
2020-04-14 16:53:39 -0300

    impressão refeita para jogo de damas

--- src/Campo.java
+++ src/Campo.java
@@ -2,15 +2,23 @@ public class Campo {
 	private int x;
 	private int y;
 	private boolean peca; //false se for um espaco vazio, true se for um espaco pra peca
-	private boolean comida; //false quanto a peÃ§a nesse espaco nao foi comida, true quando foi comida
-	private boolean preta;// false se for branca B, true se for preta P
-	
-	public Campo (int x, int y, boolean peca,boolean preta) {
+	private boolean comida; //false quanto a peça nesse espaco nao foi comida, true quando foi comida
+	private boolean preta; // false se for uma peca branca B, true se for uma peca preta P
+
+	public Campo (int x, int y, boolean peca, boolean preta) {
 		this.x = x;
 		this.y = y;
 		this.peca = peca;
 		this.comida = false;
 		this.preta = preta;
+	}
+	
+	public boolean getPeca() {
+		return peca;
+	}
+	
+	public boolean getPreta() {
+		return preta;
 	}
 	
 	public void colocarPeca(boolean preta) {
@@ -20,19 +28,16 @@ public class Campo {
 	}
 	
 	public void comerPeca() {
+		peca = false;
 		comida = true;
 	}
 	
 	public void mostrarCampo() {
-		if (peca == false) {
-			System.out.print("  ");
-		} else if (comida == false) {
-			if(preta == false) {
+		if (peca == true) {
+			if (preta == false)
 				System.out.print("B ");
-			}
-			else {
+			else
 				System.out.print("P ");
-			}
 		} else {
 			System.out.print("- ");
 		}
