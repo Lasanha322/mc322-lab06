497c4befae4364a3f5084eebbb8321f7fd98ccf7
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-14 16:22:03 -0300

    Update Campo.java

--- src/Campo.java
+++ src/Campo.java
@@ -2,18 +2,21 @@ public class Campo {
 	int x;
 	int y;
 	boolean peca; //false se for um espaco vazio, true se for um espaco pra peca
-	boolean comida; //false quanto a peça nesse espaco nao foi comida, true quando foi comida
+	boolean comida; //false quanto a peÃ§a nesse espaco nao foi comida, true quando foi comida
+	boolean cor;// false se for branca B, true se for preta P
 	
-	Campo (int x, int y, boolean peca) {
+	Campo (int x, int y, boolean peca,boolean cor) {
 		this.x = x;
 		this.y = y;
 		this.peca = peca;
 		this.comida = false;
+		this.cor = cor;
 	}
 	
-	void colocarPeca() {
+	void colocarPeca(boolean cor) {
 		peca = true;
 		comida = false;
+		this.cor = cor;
 	}
 	
 	void comerPeca() {
@@ -24,7 +27,12 @@ public class Campo {
 		if (peca == false) {
 			System.out.print("  ");
 		} else if (comida == false) {
-			System.out.print("P ");
+			if(cor == false) {
+				System.out.print("B ");
+			}
+			else {
+				System.out.print("P ");
+			}
 		} else {
 			System.out.print("- ");
 		}
