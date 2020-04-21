578e1a560458e66dfb5eaeffd7fc973f071858b1
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-21 16:08:26 -0300

    Update Comum.java

--- src/Comum.java
+++ src/Comum.java
@@ -7,34 +7,51 @@ public class Comum extends Campo {
 		// TODO Auto-generated constructor stub
 	}
 	public boolean movimento(int x, int y, Campo [][] camp) {
+		boolean muv =false;
+		int fator;
+		if(peca== true && comida == false) {
+			// verifica se a peca sobe ou desce 
+			if(getPreta() == false) {
+				fator =1;
+				muv = verificaMovimento (x, y, camp, fator);
+			}
+			else {
+				fator = -1;
+				muv = verificaMovimento (x, y, camp, fator);
+			}
+		}
+		return muv;
+	}
+	public boolean verificaMovimento (int x, int y, Campo [][] camp, int fator) {
 		boolean muv=false;
-		// verifica se movimento de uma casa é verdadeiro
-			if(y==(this.y+1)) {
-				if(x==(this.x+1) || x==(this.x-1) ) {
-					if(camp[x][y].comida == true) {
+		int lado ;
+		// verifica se movimento de 1 casa é verdadeiro
+		lado = 1*fator;
+		if(y==(this.y+lado)) {
+			if(x==(this.x+1) || x==(this.x-1) ) {
+				if(camp[x][y].comida == true) {
+					muv=true;
+				}
+			}
+		}
+		//verifica se o movimento de 2 espaço é possivel e se entre eles tem uma peça != da propria
+			
+		if(y==(this.y+(2*lado))) {
+			if(x==(this.x+2) ) {
+				if(camp[x][y].comida == true) {
+					if(camp[this.x+1][this.y+lado].comida==false && camp[this.x+1][this.y+lado].preta !=preta){
 						muv=true;
 					}
 				}
 			}
-		//verifica se o movimento de 2 espaço é possivel e se entre eles tem uma peça != da propria
-			if(y==(this.y+2)) {
-				if(x==(this.x+2) ) {
-					if(camp[x][y].comida == true) {
-						if(camp[x-1][y-1].comida==false && camp[x-1][y-1].preta !=preta) {
-							muv=true;
-						}
-						
-					}
-				}
-				if(x==(this.x-2) ) {
-					if(camp[x][y].comida == true) {
-						if(camp[x+1][y-1].comida==false && camp[x+1][y-1].preta !=preta) {
-							muv=true;
-						}
-						
-					}
+			if(x==(this.x-2) ) {
+				if(camp[x][y].comida == true) {
+					if(camp[this.x-1][this.y+lado].comida==false && camp[this.x-1][this.y+lado].preta !=preta){
+						muv=true;
+					}	
 				}
 			}
+		}
 		return muv;
 	}
 }
