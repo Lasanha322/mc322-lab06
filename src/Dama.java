2a6218e970533d6e42a68dea81c5091bb70cbe8c
CauanNewton <63312517+CauanNewton@users.noreply.github.com>
2020-04-23 20:22:10 -0300

    Add files via upload

--- src/Dama.java
+++ src/Dama.java
@@ -4,74 +4,76 @@ public class Dama extends Campo {
 		super(x, y);
 		this.preta = preta;
 	}
-
-	public boolean movimento(int x, int y, Campo[][] camp) {
+	
+	public boolean movimento(int x, int y, Campo [][] camp) {
 		boolean muv = false;
-		int fatorX;// direcao em x
-		int fatorY;// direcaoo em y
-		// definindo a direcao do movimento
-		// ideia de quadrante, pense que a peca esta no centro e
-		// o x e y vao indicar para que direcao esta indo a peca
-		if (y > this.y) {
-			if (x > this.x) {
-				fatorX = 1;
-				fatorY = 1;
-				muv = verificaMovimento(x, y, camp, fatorX, fatorY);
-			} else if (x < this.x) {
-				fatorX = -1;
-				fatorY = 1;
-				muv = verificaMovimento(x, y, camp, fatorX, fatorY);
-			}
-		}
-		if (y < this.y) {
-			if (x > this.x) {
-				fatorX = 1;
-				fatorY = -1;
-				muv = verificaMovimento(x, y, camp, fatorX, fatorY);
-			} else if (x < this.x) {
-				fatorX = -1;
-				fatorY = -1;
-				muv = verificaMovimento(x, y, camp, fatorX, fatorY);
-			}
-		}
+		int fatorX;//direcao em x
+		int fatorY;//direcao em y
+		//definindo a direcao do movimento
+		//ideia de quadrante, pense que a peca esta no centro e 
+		//o x e y vao indicar para que direcao esta indo a peca
+		
+		if (x > this.x)
+			fatorX = 1;
+		else
+			fatorX = -1;
+		
+		if (y > this.y)
+			fatorY = 1;
+		else
+			fatorY = -1;
+
+		muv = verificaMovimento(x, y, camp, fatorX, fatorY);		
 		return muv;
 	}
-
-	public boolean verificaMovimento(int x, int y, Campo[][] camp, int fatorX, int fatorY) {
+	
+	public boolean verificaMovimento (int x, int y, Campo [][] camp, int fatorX, int fatorY) {
 		boolean muv = false;
 		int somadorX, somadorY;
-
 		if (fatorX < 0)
-			somadorX = -1 * (fatorX / fatorX); // somador para ir para o proximo campo
-		else
-			somadorX = fatorX / fatorX;
-
-		if (fatorY < 0)
-			somadorY = -1 * (fatorY / fatorY);
-		else
-			somadorY = fatorY / fatorY;
-
-		// verifica cada campo ate chegar na posicao final desejada
-		while (x != (this.x + fatorX) && y != (this.y + fatorY)) {
-			if (camp[this.x + (fatorX)][this.y + (fatorY)].getComum() != null
-					&& camp[this.x + (fatorX)][this.y + (fatorY)].comum.preta != preta
-					|| camp[this.x + (fatorX)][this.y + (fatorY)].getDama() != null
-							&& camp[this.x + (fatorX)][this.y + (fatorY)].dama.preta != preta) {
-				if (camp[this.x + (fatorX + somadorX)][this.y + (fatorY + somadorY)].getComum() == null
-						&& camp[this.x + (fatorX)][this.y + (fatorY)].getDama() == null) {
-					muv = true;
+			somadorX = -1; //somador para ir para o proximo campo
+		else 
+			somadorX = 1;
+		
+		if(fatorY < 0)
+			somadorY= -1;
+		else 
+			somadorY = 1;
+		
+		//verifica cada campo ate chegar na posicao final desejada
+		while(x != this.x+fatorX && y != this.y+fatorY) {
+			// Se o a proximo campo estiver vazio, ele chama a funcao novamente
+			if (camp[this.x+fatorX][this.y+fatorY].getComum()==null && camp[this.x+fatorX][this.y+fatorY].getDama() == null) {
+				fatorX = fatorX + somadorX;
+				fatorY = fatorY + somadorY;
+			} else {
+				 //se nao estiver vazio, verifica se eh peca adversaria 
+				if(camp[this.x+fatorX][this.y+fatorY].getComum() != null && 
+					camp[this.x+fatorX][this.y+fatorY].comum.getPreta() != this.preta ||
+					camp[this.x+fatorX][this.y+fatorY].getDama() != null &&
+					camp[this.x+fatorX][this.y+fatorY].dama.getPreta() != this.preta) {
+					fatorX = fatorX + somadorX;
+					fatorY = fatorY + somadorY;					
+					//se for peca adversaria, verifica se o campo apos o adversario esta vazio
+					if(camp[this.x+fatorX][this.y+fatorY].getComum() == null &&
+							camp[this.x+fatorX][this.y+fatorY].getDama() == null ) {
+						//se este campo for o destino final, retorna que eh jogada valida
+						if(this.x+fatorX == x && this.y+fatorY == y) {
+							muv=true;
+						}
+					}
 				}
 			}
-			fatorX = fatorX + somadorX;
-			fatorY = fatorY + somadorY;
 		}
-		// se a peca passar por todos os campos e a posicao destino ser igual a
-		// verificada, verifica se o espaco esta disponivel
-		if (x == (this.x + fatorX) && y == (this.y + fatorY)){
-			if (camp[x][y].getComum() == null && camp[x][y].getDama() == null) {
-				muv = true;
-			}
+		
+		//se a peca passar por todos os campos e a posicao destino ser igual a 
+		//verificada, verifica se o espaco esta disponivel
+		if(x== this.x + fatorX && y==this.y + fatorY){
+			if(camp[x][y].getComum() == null && camp[x][y].getDama() == null) {
+				muv=true;
+			}		
 		}
+		
 		return muv;
 	}
 }
